1075620f2dfb07e434c91f0e73b80c295e433ef7
UnplugCharger <isaacbyron@gmail.com>
2024-05-09 10:20:15 +0300

    pipeline failling tests resolved

--- db/sqlc/querries.sql_test.go
+++ db/sqlc/querries.sql_test.go
@@ -258,12 +258,12 @@ func TestListTasks(t *testing.T) {
 		createRandomTask(t)
 	}
 
-	args := ListTasksParams{
-		Limit:  5,
-		Offset: 5,
-	}
+	//args := ListTasksParams{
+	//	Limit:  5,
+	//	Offset: 5,
+	//}
 
-	tasks, err := testStore.ListTasks(context.Background(), args)
+	tasks, err := testStore.ListTasks(context.Background())
 	require.NoError(t, err)
 	require.Len(t, tasks, 5)
 	for _, task := range tasks {
